e6ee1e02ba03af18bf85fe17a529681dba6d889d
Aoz0ra <64323569+Aoz0ra@users.noreply.github.com>
2020-07-23 17:39:17 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,18 +18,18 @@ function saveAccent(newColor) {
 	localStorage.setItem(`accentColor`, newColor);
 }
 
-function setTheme(theme) {
+function saveTheme(theme) {
 	if (theme == `default`) {
 		localStorage.setItem(`themeToLoad`, ``)
 	}
 	
 	else if (theme == `Solarized`) {
-		localStorage.setItem(`themeToLoad`, `/hydrogen-framework/themes/aoz0ras-solarized-dark.css`)
+		localStorage.setItem(`themeToLoad`, `<link rel="stylesheet" href="/hydrogen-framework/themes/aoz0ras-solarized-dark.css">`)
 	}
 }
 
 function loadTheme() {
-	$(`head`).append(`<link rel="stylesheet" href="${localStorage.themeToLoad}">`)
+	$(`head`).append(`${localStorage.themeToLoad}`)
 }
 
 
@@ -40,4 +40,12 @@ if (!localStorage.accentColor) {
 }
 else {
 	changeAccent(localStorage.getItem(`accentColor`))
+}
+
+// Make sure that an empty string doesn't cause the theme to be reset, just in case
+if (!localStorage.themeToLoad && localStorage.themeToLoad !== ``) {
+	saveTheme(`default`);
+}
+else {
+	loadTheme(localStorage.getItem(`themeToLoad`))
 }
