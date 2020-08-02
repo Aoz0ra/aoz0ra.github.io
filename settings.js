26aeb709bb0a156d2dc80a54460b064f1d89052b
Aoz0ra <64323569+Aoz0ra@users.noreply.github.com>
2020-08-02 13:08:30 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ function getInputValue(id) {
 
 
 
-$(`#dropContent`).html(`<a href="/hydrogen-framework/index.html">Hydrogen</a>`+
-		      `<a href="/winclassic/index.html">WinClassic</a>`+
-		      `<a href="/settings/index.html">Settings</a>`)
+$(`#dropContent`).html(`<div class="item> <a href="/hydrogen-framework/index.html">Hydrogen</a> </div>`+
+		      `<div class="item> <a href="/winclassic/index.html">WinClassic</a> </div>`+
+		      `<div class="item> <a href="/settings/index.html">Settings</a> </div>`)
 
 
 
