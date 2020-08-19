b29171cb373cd00ace134465a0f0bfbc53df33a9
Aoz0ra <64323569+Aoz0ra@users.noreply.github.com>
2020-08-19 16:06:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -28,15 +28,17 @@ String.prototype.findReplace = function(find, replace, caseSensitiveBool, failsa
 
 
 var templates = {
-	'[[TEST_TEMPLATE]]':      `<button>Congratulations, this test template works.</button>`,
+'[[TEST_TEMPLATE]]':      `<button>Congratulations, this test template works.</button>`,
 
-	'[[SONIC_DOESNT_CARE]]':  `<div class="card" style="text-align: center"><p style="color: var(--blue);">Woah! That's interesting, but I sure don't care!</p><p>While this page may have some interesting content, it's not part of the main stuff here.</p></div>`,
-	'[[THIS_IS_HAPPENIN]]':   `<div class="card" style="text-align: center"><p style="color: var(--blue);">Aw yeah!  This is happenin'!</p><p>[will fill out the text later]</p></div>`,
-	'[[AU_SCARY]]':           `<div class="card" style="text-align: center"><p style="color: var(--orange);">[cream is scared]</p><p>[will fill out the text later]</p></div>`,
-	'[[AU_SCARY_VERY]]':      `<div class="card" style="text-align: center"><p style="color: var(--orange);">[cream is scared for her life]</p><p>[will fill out the text later]</p></div>`,
-	'[[NO_ONLINE_PRESENCE]]': `<div class="card" style="text-align: center"><p style="color: var(--skyblue);">...Where is everyone?</p><p>This AU has no online presence other than this very page.</p></div>`,
-	'[[CONJECTURAL_NAME]]':   `<div class="card" style="text-align: center"><p style="color: var(--red);">I need to find the Chaos Emerald to reveal the secrets about my past...</p><p>The real name of this is unknown, and as such this temporary name is being used instead.</p></div>`,
-	'[[DELETE_THIS]]':        `<div class="card" style="text-align: center"><p style="color: var(--gray);">The strong will always vanquish the weak</p><p>This page should be deleted.  Consider migrating its content elsewhere.</p></div>`,
+'[[SONIC_DOESNT_CARE]]':  `<div class="card" style="text-align: center"><blockquote><p style="color: var(--blue);">Woah! That's interesting, but I sure don't care!</p><footer><p>— Sonic</p></footer></blockquote><p>While this page may have some interesting content, it's not part of the main stuff here.</p></div>`,
+'[[THIS_IS_HAPPENIN]]':   `<div class="card" style="text-align: center"><blockquote><p style="color: var(--blue);">Aw yeah!  This is happenin'!</p><footer><p>— Sonic</p></footer></blockquote><p>[will fill out the text later]</p></div>`,
+'[[AU_SCARY]]':           `<div class="card" style="text-align: center"><blockquote><p style="color: var(--orange);">[cream is scared]</p><footer><p>— Cream</p></footer></blockquote><p>[will fill out the text later]</p></div>`,
+'[[AU_SCARY_VERY]]':      `<div class="card" style="text-align: center"><blockquote><p style="color: var(--orange);">[cream is scared for her life]</p><footer><p>— Cream</p></footer></blockquote><p>[will fill out the text later]</p></div>`,
+'[[NO_ONLINE_PRESENCE]]': `<div class="card" style="text-align: center"><blockquote><p style="color: var(--skyblue);">...Where is everyone?</p><footer><p>— Silver</p></footer></blockquote><p>This AU has no online presence other than this very page.</p></div>`,
+'[[CONJECTURAL_NAME]]':   `<div class="card" style="text-align: center"><blockquote><p style="color: var(--red);">I need to find the Chaos Emerald to reveal the secrets about my past...</p><footer><p>— Shadow</p></footer></blockquote><p>The real name of this is unknown, and as such this temporary name is being used instead.</p></div>`,
+'[[DELETE_THIS]]':        `<div class="card" style="text-align: center"><blockquote><p style="color: var(--gray);">The strong will always vanquish the weak!</p><footer><p>— Infinite, Sonic Forces</p></footer></blockquote><p>This page should be deleted.  Consider checking what links to this page and migrating its content elsewhere.</p></div>`,
+'[[STUB]]':               `<div class="card" style="text-align: center"><blockquote><p style="color: var(--orange);">There's not a whole lot of information on this right now...</p><footer><p>— Tails</p></footer></blockquote><p>This page is a stub.  Please help by expanding it!</p></div>`,
+
 }
 
 var finishedPageHTML = JSON.stringify($(`div.page`).html()).findReplace(`\\n`, ``).findReplace(`\\t`, ``).findReplace(`\\"`, `"`)
