34a9487a8d31d62ee6d34b008e7a887d798507ee
Aoz0ra <64323569+Aoz0ra@users.noreply.github.com>
2020-08-19 15:00:48 -0400

    Update main.js

--- main.js
+++ main.js
@@ -29,11 +29,12 @@ String.prototype.findReplace = function(find, replace, caseSensitiveBool, failsa
 
 var templates = {
 	'[[TEST_TEMPLATE]]': `<button>Congratulations, this test template works.</button>`,
-	'[[SONIC_DOESNT_CARE]]': `<div class="card"><p style="color: var(--blue)">Woah! That's interesting, but I sure don't care!</p><p>While this page may have some interesting content, it's not part of the main stuff here.</p></div>`,
-	'[[THIS_IS_HAPPENIN]]': `<div class="card"><p style="color: var(--blue)">Aw yeah!  This is happenin'!</p><p>[will fill out the text later]</p></div>`,
-	'[[AU_SCARY]]': `<div class="card"><p style="color: var(--orange)">[cream is scared]</p><p>[will fill out the text later]</p></div>`,
-	'[[AU_SCARY_VERY]]': `<div class="card"><p style="color: var(--orange)">[cream is scared for her life]</p><p>[will fill out the text later]</p></div>`,
-	'[[NO_ONLINE_PRESENCE]]': `<div class="card"><p style="color: var(--skyblue)">...Where is everyone?</p><p>This AU has no online presence other than this very page.</p></div>`,
+	'[[SONIC_DOESNT_CARE]]': `<div class="card"><p style="color: var(--blue); text-align: center;">Woah! That's interesting, but I sure don't care!</p><p>While this page may have some interesting content, it's not part of the main stuff here.</p></div>`,
+	'[[THIS_IS_HAPPENIN]]': `<div class="card"><p style="color: var(--blue); text-align: center;">Aw yeah!  This is happenin'!</p><p>[will fill out the text later]</p></div>`,
+	'[[AU_SCARY]]': `<div class="card"><p style="color: var(--orange); text-align: center;">[cream is scared]</p><p>[will fill out the text later]</p></div>`,
+	'[[AU_SCARY_VERY]]': `<div class="card"><p style="color: var(--orange); text-align: center;">[cream is scared for her life]</p><p>[will fill out the text later]</p></div>`,
+	'[[NO_ONLINE_PRESENCE]]': `<div class="card"><p style="color: var(--skyblue); text-align: center;">...Where is everyone?</p><p>This AU has no online presence other than this very page.</p></div>`,
+	'[[CONJECTURAL_NAME]]': `<div class="card"><p style="color: var(--red); text-align: center;">I need to find the Chaos Emerald to reveal the secrets about my past...</p><p>The real name of this is unknown, and as such this temporary name is being used instead.</p></div>`,
 }
 
 var finishedPageHTML = JSON.stringify($(`div.page`).html()).findReplace(`\\n`, ``).findReplace(`\\t`, ``).findReplace(`\\"`, `"`)
